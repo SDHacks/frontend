d1a095819efd62d1bc8db04aa9b10767840d0ae6
Joseph Le <josephle@me.com>
2014-09-22 16:22:21 -0700

    Send button for contact us triggers thank you message

--- app/scripts/sdhacks.js
+++ app/scripts/sdhacks.js
@@ -5,8 +5,17 @@ var app = angular.module('sdhacks', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSan
 app.config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
+      abstract: true,
       url: '/',
       templateUrl: 'partials/main.html'
+    })
+    .state('home.contact', {
+      url: '',
+      templateUrl: 'partials/contact/form.html'
+    })
+    .state('home.contact-submit', {
+      url: '',
+      template: '<h4 style="text-align: center;">Thank you for your message!</h4>'
     })
     .state('register', {
       url: '/register',
