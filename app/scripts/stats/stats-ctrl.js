63b08c553944580adc93f07296ee5b8cf6eeebd8
Joseph Le <josephle@me.com>
2014-09-23 02:04:07 -0700

    Added rounded down company number and '+' to hackers and companies

--- app/scripts/stats/stats-ctrl.js
+++ app/scripts/stats/stats-ctrl.js
@@ -9,9 +9,9 @@ app.controller('StatsCtrl',
 
     var stats = {
       hours: 36,
-      hackers: 1000,
-      companies: Sponsors.getCount(),
-      'in prizes': '$20000'
+      hackers: 1000 + '+',
+      companies: 5*(Math.floor(Math.abs(Sponsors.getCount()/5))) + '+',
+      'in prizes': '$1,000s'
     };
 
     $scope.stats = stats;
